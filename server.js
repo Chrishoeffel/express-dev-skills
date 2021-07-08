2305ad0badcc4b7860172a997119028e477aab19
Chris Ladd <codebyladd@gmail.com>
2021-07-08 00:52:48 -0400

    add views/home.ejs with template

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 /*======== System Variables =======*/
 const PORT = 4000;
 /*======== App Configurations =======*/
+app.set('view engine', 'ejs');
 /*======== Routes =======*/
+app.get('/', function(req, res) {
+    res.send('<h1>Hello Express</h1>');
+  });
 /*======== Server Bind =======*/
 app.listen(PORT, function () {
     console.log(`Active on port: ${PORT}` )
